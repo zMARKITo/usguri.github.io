e6b4f81dc8443e82c61cc132bddbe78ec671171d
zMARKITo <viniciusbelloni21@gmail.com>
2024-01-10 14:17:57 -0300

    Site de skins privado

---
+++ class/config.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_HOST', '189.50.107.175');
+define('DB_PORT', '3306');
+define('DB_NAME', 'skin');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
+define('WEB_STYLE_DARK', true);
+
+define('STEAM_API_KEY', '182F98B9E92D620202EBBF2D7A4E635C');
+define('STEAM_DOMAIN_NAME', 'markito');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
+
